0af3c0fb7dc95c3082179efa5f709bce12f3e93b
hgirish <girish@post.com>
2020-12-06 18:21:04 -0800

    LocalStorage for Authentication & Notify about the Authentication State

--- BlazorBattles/Client/CustomAuthStateProvicer.cs
+++ BlazorBattles/Client/CustomAuthStateProvicer.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -9,15 +7,28 @@ namespace BlazorBattles.Client
 {
     public class CustomAuthStateProvicer : AuthenticationStateProvider
     {
-        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        private readonly ILocalStorageService _localStorageService;
+
+        public CustomAuthStateProvicer(ILocalStorageService localStorageService)
         {
-            var identity = new ClaimsIdentity(
+            _localStorageService = localStorageService;
+        }
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            if (await _localStorageService.GetItemAsync<bool>("isAuthenticated"))
+            {
+                var identity = new ClaimsIdentity(
                 new[]
                 {
                     new Claim(ClaimTypes.Name,"Patrick")
                 }, "test authentication type");
-            var user = new ClaimsPrincipal(identity);
-            return Task.FromResult(new AuthenticationState(user));
+                var user = new ClaimsPrincipal(identity);
+                var state = new AuthenticationState(user);
+                NotifyAuthenticationStateChanged(Task.FromResult(state));
+                return state;
+            }
+            
+            return new AuthenticationState(new ClaimsPrincipal());
         }
     }
 }
