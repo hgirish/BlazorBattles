35353d364de2b63942727016377d5bedea85e7c4
hgirish <girish@post.com>
2020-12-07 17:18:46 -0800

    Authentication Repository

---
+++ BlazorBattles/Server/Data/AuthRepository.cs
@@ -0,0 +1,23 @@
+﻿using BlazorBattles.Shared;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Server.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        public Task<string> Login(string email, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<int> Register(User user, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> UserExists(string email)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- BlazorBattles/Server/Startup.cs
+++ BlazorBattles/Server/Startup.cs
@@ -28,6 +28,7 @@ namespace BlazorBattles.Server
            x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
