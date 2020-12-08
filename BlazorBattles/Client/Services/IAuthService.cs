fb92c21b07f2ad59d6dc15f48ae9257e462d1f22
hgirish <girish@post.com>
2020-12-07 18:19:38 -0800

    Register a User on the Client

---
+++ BlazorBattles/Client/Services/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using BlazorBattles.Shared;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Client.Services
+{
+    public interface IAuthService
+    {
+        Task<ServiceResponse<int>> Register(UserRegister request);
+    }
+}
