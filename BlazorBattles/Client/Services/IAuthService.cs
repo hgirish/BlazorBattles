4bf1812a9771a817ec68ce0983424e69bc8cfd4f
hgirish <girish@post.com>
2020-12-07 18:40:39 -0800

    Login with the client

--- BlazorBattles/Client/Services/IAuthService.cs
+++ BlazorBattles/Client/Services/IAuthService.cs
@@ -6,5 +6,6 @@ namespace BlazorBattles.Client.Services
     public interface IAuthService
     {
         Task<ServiceResponse<int>> Register(UserRegister request);
+        Task<ServiceResponse<string>> Login(UserLogin request);
     }
 }
