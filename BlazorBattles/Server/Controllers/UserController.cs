40af044cadf2928298ce083c1053420330a15b11
hgirish <girish@post.com>
2020-12-08 21:20:25 -0800

    Refactor with a Utility Service

--- BlazorBattles/Server/Controllers/UserController.cs
+++ BlazorBattles/Server/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using BlazorBattles.Server.Data;
+using BlazorBattles.Server.Services;
 using BlazorBattles.Shared;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -17,32 +18,32 @@ namespace BlazorBattles.Server.Controllers
     public class UserController : ControllerBase
     {
         private readonly DataContext _context;
+        private readonly IUtilityService _utilityService;
 
-        public UserController(DataContext context)
+        public UserController(DataContext context, IUtilityService utilityService)
         {
             _context = context;
+            _utilityService = utilityService;
         }
 
         [HttpGet("GetBananas")]
         public async Task<IActionResult> GetBananas()
         {
-            var user = await GetUser();
+            var user = await _utilityService.GetUser();
 
             return Ok(user.Bananas);
         }
         [HttpPut("AddBananas")]
         public async Task<IActionResult> AddBananas([FromBody] int bananas)
         {
-            var user = await GetUser();
+            var user = await _utilityService.GetUser();
             user.Bananas += bananas;
             await _context.SaveChangesAsync();
             return Ok(user.Bananas);
         }
 
-        private int GetUserId() => int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
-
-        private async Task<User> GetUser() => await _context.Users.FirstOrDefaultAsync(
-            u => u.Id == GetUserId());
+       
+        
 
     }
 }
--- BlazorBattles/Server/Startup.cs
+++ BlazorBattles/Server/Startup.cs
@@ -1,7 +1,9 @@
 using BlazorBattles.Server.Data;
+using BlazorBattles.Server.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
@@ -35,7 +37,7 @@ namespace BlazorBattles.Server
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
                 {
-                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    options.TokenValidationParameters = new TokenValidationParameters
                     {
                         ValidateIssuerSigningKey = true,
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(
@@ -44,6 +46,8 @@ namespace BlazorBattles.Server
                         ValidateAudience = false
                     };
                 });
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IUtilityService, UtilityService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
