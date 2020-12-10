1b24b66c172b9e87b8e44b31539de6e18306922a
hgirish <girish@post.com>
2020-12-09 20:54:28 -0800

    Fight battles in Blazor WebAssembly

---
+++ BlazorBattles/Client/Services/IBattleService.cs
@@ -0,0 +1,10 @@
+﻿using BlazorBattles.Shared;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Client.Services
+{
+    public interface IBattleService
+    {
+        Task<BattleResult> StartBattle(int opponentId);
+    }
+}
